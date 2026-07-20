41734c7e54a4012420672f01f34933a4dbd8ef84
Zahra <Z.Mahdinezhad125@gmail.com>
2026-07-20 11:15:06 +0330

    Implement Playlist entity

--- Entities/playlist.cpp
+++ Entities/playlist.cpp
@@ -1,3 +1,33 @@
 #include "playlist.h"
 
-Playlist::Playlist() {}
+Playlist::Playlist()
+{
+    this->playlistID=0;
+    this->listenerID=0;
+}
+Playlist::Playlist(int pLID, const QString& name, int lID)
+{
+    this->playlistID=pLID;
+    this->name=name;
+    this->listenerID=lID;
+}
+int Playlist::getPlaylistID()const
+{
+    return this->playlistID;
+}
+QString Playlist::getName()const
+{
+    return this->name;
+}
+int Playlist::getListenerID()const
+{
+    return this->listenerID;
+}
+QVector<int> Playlist::getSongIDs()const
+{
+    return this->songIDs;
+}
+void Playlist::setName(const QString& name)
+{
+    this->name=name;
+}
--- Entities/playlist.h
+++ Entities/playlist.h
@@ -1,10 +1,23 @@
 #ifndef PLAYLIST_H
 #define PLAYLIST_H
 
+#include <QString>
+#include <QVector>
 class Playlist
 {
+private:
+    QString name;
+    int listenerID;
+    int playlistID;
+    QVector<int> songIDs;
 public:
     Playlist();
+    Playlist(int pLID, const QString& name, int lID);
+    void setName(const QString& name);
+    QString getName()const;
+    int getListenerID()const;
+    int getPlaylistID()const;
+    QVector <int> getSongIDs()const;
 };
 
 #endif // PLAYLIST_H
