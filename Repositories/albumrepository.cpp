aa9570b2415ce089e485a2fdd47b342eda20456c
Zahra <Z.Mahdinezhad125@gmail.com>
2026-07-20 22:16:50 +0330

    Implement AlbumRepository

---
+++ Repositories/albumrepository.cpp
@@ -0,0 +1,3 @@
+#include "albumrepository.h"
+
+AlbumRepository::AlbumRepository() {}
---
+++ Repositories/albumrepository.h
@@ -0,0 +1,10 @@
+#ifndef ALBUMREPOSITORY_H
+#define ALBUMREPOSITORY_H
+
+class AlbumRepository
+{
+public:
+    AlbumRepository();
+};
+
+#endif // ALBUMREPOSITORY_H
